3c651abff29f3d3e50d38ee8008425191e14c310
Michel <mchl.fran04@gmail.com>
2025-02-27 17:15:39 +0100

    27/02/25

--- test3bub/Assets/Scripts/PlayerController.cs
+++ test3bub/Assets/Scripts/PlayerController.cs
@@ -9,17 +9,83 @@ using UnityEngine.InputSystem;
 public class PlayerController : Entity
 {
     [SerializeField] private Rigidbody2D playerRigidbody;
+
+    [SerializeField] private Animator playerAnimator;
+
+    public string[] allAnimations = new[] { "isRunning", "isJumping", "isFalling", "isWalking" };
+
+    enum PlayerState
+    {
+        Idle,
+        Walk,
+        Run,
+        Jump,
+        Fall
+    }
+
+    private PlayerState currentState = PlayerState.Idle;
+
+    private void ChangeState(PlayerState newState)
+    {
+        currentState = newState;
+        switch (currentState)
+        {
+            case PlayerState.Idle : 
+                ResetAllAnimatorExept("");
+                break;
+            case PlayerState.Walk :
+                playerAnimator.SetBool("isWalking", true);
+                ResetAllAnimatorExept("isWalking");
+                break;
+            case PlayerState.Run :
+                playerAnimator.SetBool("isRunning", true);
+                ResetAllAnimatorExept("isRunning");
+                break;
+            case PlayerState.Jump :
+                playerAnimator.SetBool("isJumping", true);
+                ResetAllAnimatorExept("isJumping");
+                break;
+            case PlayerState.Fall :
+                playerAnimator.SetBool("isFalling", true);
+                ResetAllAnimatorExept("isFalling");
+                break;
+        }
+    }
+
+    private void ResetAllAnimatorExept(string animationName)
+    {
+        foreach (var animName in allAnimations)
+        {
+            if (animName == animationName) continue;
+            playerAnimator.SetBool(animName, false);
+        }
+    }
     
     public void Movement(InputAction.CallbackContext horizontalValue)
     {
         if (Mathf.Abs(horizontalValue.ReadValue<float>()) <= 0.5 && !IsOnGround()) return;
         
         playerRigidbody.linearVelocityX = horizontalValue.ReadValue<float>() * currentEntityData.speed;
+
+        if (currentState == PlayerState.Fall || currentState == PlayerState.Jump) return;
+        if (Mathf.Abs(horizontalValue.ReadValue<float>()) == 0)
+        {
+            ChangeState(PlayerState.Idle);
+        }
+        else if (Mathf.Abs(horizontalValue.ReadValue<float>()) <= 0.6)
+        {
+            ChangeState(PlayerState.Walk);
+        }
+        else
+        {
+            ChangeState(PlayerState.Run);
+        }
+        
         
     }
     
-    private float jumpTime = 0f; // Temps pendant lequel le bouton est appuyé
-    private bool isJumping = false; // Indicateur pour savoir si le joueur est en train de sauter
+    private float jumpTime = 0f;
+    private bool isJumping = false;
     private const float MAX_JUMP_TIME = 0.15f;
     
     private Vector2 currentJumpForce;
@@ -34,9 +100,20 @@ public class PlayerController : Entity
             
             currentJumpForce = Vector2.Lerp(currentJumpForce, Vector2.zero, Time.deltaTime);
             playerRigidbody.AddForce(currentJumpForce);
-            Debug.Log("a");
         }
-        
+
+        if (playerRigidbody.linearVelocityY < 0)
+        {
+            ChangeState(PlayerState.Fall);
+        }
+
+        if (currentState == PlayerState.Fall)
+        {
+            if (IsOnGround())
+            {
+                ChangeState(PlayerState.Idle);
+            }
+        }
         
     }
 
@@ -49,6 +126,7 @@ public class PlayerController : Entity
                 isJumping = true;
                 jumpTime = 0;
                 currentJumpForce = new Vector2(0, currentEntityData.jumpPower);
+                ChangeState(PlayerState.Jump);
             }
         }
 
@@ -73,8 +151,7 @@ public class PlayerController : Entity
     {
         
         if (shootAngle != Vector2.zero)
-        { 
-            Debug.Log(shootAngle);
+        {
             Rigidbody2D projectileRb = Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, Mathf.Atan2(shootAngle.y, shootAngle.x) * Mathf.Rad2Deg)).GetComponent<Rigidbody2D>();
             projectileRb.linearVelocity = shootAngle * currentEntityData.projectileSpeed;
         }
